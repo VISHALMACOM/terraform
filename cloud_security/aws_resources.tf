35259eedeba7ea15ee00e1b3e4ce24cc9c40b8f2
VISHALMACOM <vishalmacom3@gmail.com>
2024-03-11 18:43:54 -0400

    security

---
+++ cloud_security/aws_resources.tf
@@ -0,0 +1,21 @@
+
+
+resource "aws_instance" "lab6_security" {
+  ami           = "ami-0f403e3180720dd7e"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name        = "Lab6Instance"
+    Environment = "Production"
+    Owner       = "Jishnu C."
+  }
+iam_instance_profile = aws_iam_instance_profile.example_profile.name
+}
+
+resource "aws_iam_instance_profile" "example_profile" {
+  name = "example_profile"
+  role = aws_iam_role.example_role.name
+}
+
+
+ 
